ad6ac24d36b8731353bb18c2bb17def50ddf7769
Kajornthep Piyanun <kachontep@gmail.com>
2023-07-07 16:50:16 +0000

    Fix jupyter server restarting make it requests for token again

--- scripts/init-jupyter-server.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-jupyter notebook --generate-config
-sed -i "s/^# c.NotebookApp.token = .*/c.NotebookApp.token = ''/" $HOME/.jupyter/jupyter_notebook_config.py
--- scripts/start-jupyter-server.sh
+++ scripts/start-jupyter-server.sh
@@ -6,4 +6,8 @@ if [ ! -e "${NOTEBOOK_DIR}" ]; then
     mkdir -p $NOTEBOOK_DIR
 fi 
 
+# Disable token for accessing jupyter server
+jupyter notebook --generate-config -y
+sed -i "s/^# c.NotebookApp.token = .*/c.NotebookApp.token = ''/" $HOME/.jupyter/jupyter_notebook_config.py
+
 jupyter notebook --no-browser --ip 0.0.0.0 --notebook-dir=${NOTEBOOK_DIR}
