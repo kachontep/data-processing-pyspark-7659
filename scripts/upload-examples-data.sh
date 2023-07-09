adab96c8f7fa265a9fb76e4bff14d119a5c9ca89
Kajornthep Piyanun <kachontep@gmail.com>
2023-07-09 06:59:23 +0000

    Upload examples to minio script

---
+++ scripts/upload-examples-data.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+BASEDIR=$(dirname $0)
+
+# Create minio alias if non-existent
+mc alias set minio http://localhost:9000 admin adminadmin
+
+# Create bucket for examples
+mc mb -p datalake
+
+# Extract and upload data
+unzip -o -d $BASEDIR/../resources/data  $BASEDIR/../resources/data.zip
+
+pushd $BASEDIR/../resources/data
+mc cp ./* minio/datalake/examples/
+popd
+
+rm -rf $BASEDIR/../resources/data
