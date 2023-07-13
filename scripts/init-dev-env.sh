1d4a8adce4427f1cc4efab4aae43cfadb400080e
Kajornthep Piyanun <kachontep@gmail.com>
2023-07-13 11:31:27 +0000

    Initialize minio alias

--- scripts/init-dev-env.sh
+++ scripts/init-dev-env.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#
+# Spark
+#
+
+# Copy spark additional dependencies to $SPARK_HOME/jars
 SPARK_DEPS_DIR="./spark-deps"
 
 mkdir -p $SPARK_DEPS_DIR
@@ -9,3 +14,9 @@ mvn dependency:copy -Dartifact=org.apache.hadoop:hadoop-aws:3.3.1:jar -DoutputDi
 mvn dependency:copy -Dartifact=com.amazonaws:aws-java-sdk-bundle:1.11.901:jar -DoutputDirectory=$SPARK_DEPS_DIR
 
 sudo cp $SPARK_DEPS_DIR/*.jar $SPARK_HOME/jars
+
+#
+# MinIO
+
+# Create minio alias if non-existent
+mc alias set minio http://localhost:9000 admin adminadmin
--- scripts/upload-examples-data.sh
+++ scripts/upload-examples-data.sh
@@ -4,9 +4,6 @@ set -e
 
 BASEDIR=$(dirname $0)
 
-# Create minio alias if non-existent
-mc alias set minio http://localhost:9000 admin adminadmin
-
 # Create bucket for examples
 mc mb -p minio/datalake
 
