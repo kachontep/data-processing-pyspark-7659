49039433fba27898240bcdff2ba8e2bd56c568d8
Kajornthep Piyanun <kachontep@gmail.com>
2023-07-21 10:58:37 +0000

    Add iceberg dependencies

--- scripts/init-dev-env.sh
+++ scripts/init-dev-env.sh
@@ -16,6 +16,12 @@ mvn dependency:copy -Dartifact=com.amazonaws:aws-java-sdk-bundle:1.11.901:jar -D
 # Dependencies for MySQL connector
 mvn dependency:copy -Dartifact=com.mysql:mysql-connector-j:8.0.33:jar -DoutputDirectory=$SPARK_DEPS_DIR
 
+# Dependencies for Iceberg
+mvn dependency:copy -Dartifact=org.apache.iceberg:iceberg-spark-runtime-3.2_2.12:1.3.0:jar -DoutputDirectory=$SPARK_DEPS_DIR
+mvn dependency:copy -Dartifact=software.amazon.awssdk:bundle:2.18.41:jar -DoutputDirectory=$SPARK_DEPS_DIR
+mvn dependency:copy -Dartifact=software.amazon.awssdk:url-connection-client:2.18.41:jar -DoutputDirectory=$SPARK_DEPS_DIR
+mvn dependency:copy -Dartifact=software.amazon.awssdk:utils:2.18.41:jar -DoutputDirectory=$SPARK_DEPS_DIR
+
 sudo cp $SPARK_DEPS_DIR/*.jar $SPARK_HOME/jars
 
 #
